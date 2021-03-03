f28a902ea21edfea6bc3c00637245873118b8268
D0ANT <70179241+D0ANT@users.noreply.github.com>
2021-03-03 15:44:13 +0900

    Add files via upload

--- doant.py
+++ doant.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 import pyautogui
 import keyboard
+import sys
 
 
 window = Tk()
@@ -14,21 +15,77 @@ ent.pack()
 
 def click():
     auto_mouse_key = str(ent.get())
-    messagebox.showinfo("도개미 오토 클릭 안내", str(ent.get()) + "키를 오토 클릭 하겠습니다.\n중지는 [Ctrl + Shift + C]")
-    auto_mouse(auto_mouse_key)
+    if auto_mouse_key != "right click" and auto_mouse_key != "left click" and auto_mouse_key != "shift" and auto_mouse_key != "ctrl" and auto_mouse_key != "alt" and auto_mouse_key != "tab" and auto_mouse_key != "esc" and auto_mouse_key != "doant":
+        messagebox.showwarning("도개미 오토 클릭 경고","제대로 된 키를 입력하세요")
+    elif auto_mouse_key == "doant":
+        window.title("도개미 슈퍼 오토클릭")
+        ent.delete(0,len(ent.get()))
+        ent.insert(0,"도개미 짱")
+        messagebox.showinfo("도개미 슈퍼오토클릭 안내", "제작자는 도개미입니다.")
+    else:
+        messagebox.showinfo("도개미 오토 클릭 안내", str(ent.get()) + "키를 오토 클릭 하겠습니다.\n중지는 [Shift + C]")
+        auto_mouse(auto_mouse_key)
 
 def auto_mouse(auto_key):
+    if auto_key == "left click":
+        while True:
+            pyautogui.click(button='left')
+            pyautogui.click(button='left')
+            pyautogui.click(button='left')
+            pyautogui.click(button='left')
+            pyautogui.click(button='left')
+            pyautogui.click(button='left')
+            pyautogui.click(button='left')
+            pyautogui.click(button='left')
+            pyautogui.click(button='left')
+            pyautogui.click(button='left')
+            try:
+                if keyboard.is_pressed('ctrl') and keyboard.is_pressed('shift') and keyboard.is_pressed('c'):
+                    loopbreak()
+                else:
+                    continue
+            except:
+                sys.exit()
+                break
+    if auto_key == "right_click":
+        while True:
+            pyautogui.click(button='right')
+            pyautogui.click(button='right')
+            pyautogui.click(button='right')
+            pyautogui.click(button='right')
+            pyautogui.click(button='right')
+            pyautogui.click(button='right')
+            pyautogui.click(button='right')
+            pyautogui.click(button='right')
+            pyautogui.click(button='right')
+            try:
+                if keyboard.is_pressed('ctrl') and keyboard.is_pressed('shift') and keyboard.is_pressed('c'):
+                    loopbreak()
+                else:
+                    continue
+            except:
+                break
     while True:
+        pyautogui.press(auto_key)
+        pyautogui.press(auto_key)
+        pyautogui.press(auto_key)
+        pyautogui.press(auto_key)
+        pyautogui.press(auto_key)
+        pyautogui.press(auto_key)
+        pyautogui.press(auto_key)
+        pyautogui.press(auto_key)
         pyautogui.press(auto_key)
         try:
             if keyboard.is_pressed('ctrl') and keyboard.is_pressed('shift') and keyboard.is_pressed('c'):
-                messagebox.showinfo("오토마우스 탈출!", "오토 마우스 탈출!")
-                window.destroy()
-                break
+                loopbreak()
             else:
                 continue
         except:
             break
+def loopbreak():
+    messagebox.showinfo("오토 마우스 탈출!", "오토 마우스 탈출!")
+    sys.exit()
+    window.destroy()
     
 btn = Button(window)
 btn.config(text= "enter")
